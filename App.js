1b580673f1a7d0cda0dd92d29fb82286a7492ed0
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-08 17:02:54 +0530

    Add. header

--- App.js
+++ App.js
@@ -17,3 +17,50 @@
  *      - Links
  *
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./images/logo.png";
+const rootElemenent = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElemenent);
+
+const AllRestrauntCompenent = () => {
+  return (
+    <div className="restarunt-components">
+      <div className="resturant-card"></div>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div>search</div>
+      <AllRestrauntCompenent />
+    </div>
+  );
+};
+const Header = () => {
+  return (
+    <div className="heading">
+      <div className="logo">
+        <img src={logo} width={100}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app-layout">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+root.render(<AppLayout />);
