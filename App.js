ec8bc1560085d384744f89f3cb3928443d5e9654
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-08 14:31:48 +0530

    Add. logo and planning code

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-
-const helloWorld = <p>Hello World</p>; //JSX
-// Babel will transpile JSX(  <p>Hello World</p>; ) into
-// react element (React.createElement('p', {},"Hello World" ))
-
-root.render(helloWorld);
-
-// React, root will take the react element and it will render it and convert
-// it to HTML element and put it in root div in index.html
+/**
+ * AppLayout
+ *   - Header
+ *      - Logo
+ *      - NavItems
+ *          - Home, About, Cart
+ *
+ *   - Body
+ *      - Search
+ *      - AllRestrauntCompenent
+ *          - ResturantCard
+ *                 - Image
+ *                 - Description
+ *                 - Stars
+ *   - Footer
+ *      - Copyright
+ *      - Links
+ *
+ */
