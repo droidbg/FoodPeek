255607d4634c71fd27dfc18c8b0c6c3c46c103b1
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-15 17:37:26 +0530

    Add. api call to fetch restraunt data

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
-This is a food delivery application. 
+This is a food delivery application.
+
+For fetching of data we are using swiggy API :
+https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING
--- src/components/AllRestaurantCompenent.js
+++ src/components/AllRestaurantCompenent.js
@@ -1,26 +1,46 @@
 import Resturantcard from "./Resturantcard";
 import reslist from "../utils/res-list";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const AllRestaurantCompenent = () => {
-  const [listOfRest, setListOfRest] = useState(reslist);
+  const [filteredList, setFilteredList] = useState([]);
+  const [originalList, setOriginalList] = useState([]);
+  console.log("RENDERED");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    const list =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setOriginalList(list);
+    setFilteredList(list);
+  };
 
   return (
     <div>
       <div className="top-rated">
         <button
           onClick={() => {
-            const filterList = reslist.filter((element) => {
-              return element.info.avgRating > 4;
+            const filterList = originalList.filter((element) => {
+              return element.info.avgRating > 4.5;
             });
-            setListOfRest(filterList);
+
+            setFilteredList(filterList);
           }}
         >
           top rated resturants
         </button>
       </div>
       <div className="res-containers">
-        {listOfRest.map((data) => {
+        {filteredList.map((data) => {
           return <Resturantcard data={data} key={data.info.id} />;
         })}
       </div>
