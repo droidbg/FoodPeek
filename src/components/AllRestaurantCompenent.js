7820c09075868fcd8f7df5a2f5e072d821200b87
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-15 12:45:02 +0530

    Add. top rated restaurant

--- src/components/AllRestaurantCompenent.js
+++ src/components/AllRestaurantCompenent.js
@@ -1,12 +1,29 @@
 import Resturantcard from "./Resturantcard";
 import reslist from "../utils/res-list";
+import { useState } from "react";
 
 const AllRestaurantCompenent = () => {
+  const [listOfRest, setListOfRest] = useState(reslist);
+
   return (
-    <div className="res-containers">
-      {reslist.map((data) => {
-        return <Resturantcard data={data} key={data.info.id} />;
-      })}
+    <div>
+      <div className="top-rated">
+        <button
+          onClick={() => {
+            const filterList = reslist.filter((element) => {
+              return element.info.avgRating > 4;
+            });
+            setListOfRest(filterList);
+          }}
+        >
+          top rated resturants
+        </button>
+      </div>
+      <div className="res-containers">
+        {listOfRest.map((data) => {
+          return <Resturantcard data={data} key={data.info.id} />;
+        })}
+      </div>
     </div>
   );
 };
