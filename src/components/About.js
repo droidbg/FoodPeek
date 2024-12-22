5a7da355d5d1ad2c5ffd5019385bf897f9c7e242
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-22 16:12:07 +0530

    constructor -> render -> componentDidMount method

--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,82 @@
 import User from "./User";
 import UserCard from "./UserCard";
-import { useState } from "react";
 
-const About = () => {
-  const [isClassComp, setIsClassComp] = useState(true);
+// import { useState } from "react";
+import React from "react";
 
-  return (
-    <div className="about-page">
-      <h1>About Us</h1>
-      <div className="component-buttons">
-        <button
-          onClick={() => {
-            setIsClassComp(false);
-          }}
-        >
-          Functional
-        </button>
-        <button
-          onClick={() => {
-            setIsClassComp(true);
-          }}
-        >
-          Class
-        </button>
+// const About = () => {
+//   const [isClassComp, setIsClassComp] = useState(true);
+
+//   return (
+//     <div className="about-page">
+//       <h1>About Us</h1>
+//       <div className="component-buttons">
+//         <button
+//           onClick={() => {
+//             setIsClassComp(false);
+//           }}
+//         >
+//           Functional
+//         </button>
+//         <button
+//           onClick={() => {
+//             setIsClassComp(true);
+//           }}
+//         >
+//           Class
+//         </button>
+//       </div>
+//       {isClassComp ? (
+//         <UserCard name="Binni (from Class Component)" />
+//       ) : (
+//         <User name="Binni (from Functional Component)" age={1} />
+//       )}
+//     </div>
+//   );
+// };
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent About ");
+    this.state = {
+      isClassComp: true,
+    };
+  }
+  componentDidMount() {
+    console.log("Parent Component Did Mount ");
+  }
+
+  render() {
+    console.log("Parent Render ");
+    const { isClassComp } = this.state;
+    return (
+      <div className="about-page">
+        <h1>About Us</h1>
+        <div className="component-buttons">
+          <button
+            onClick={() => {
+              this.setState({ isClassComp: false });
+            }}
+          >
+            Functional
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ isClassComp: true });
+            }}
+          >
+            Class
+          </button>
+        </div>
+        {isClassComp ? (
+          <UserCard name="Binni (from Class Component)" />
+        ) : (
+          <User name="Binni (from Functional Component)" age={1} />
+        )}
       </div>
-      {isClassComp ? (
-        <UserCard name="Binni (from Class Component)" />
-      ) : (
-        <User name="Binni (from Functional Component)" age={1} />
-      )}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default About;
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,12 +3,17 @@ import React from "react";
 class UserCard extends React.Component {
   constructor(props) {
     super(props);
+    console.log("Child Constructer");
     this.state = {
       count: 0,
     };
   }
+  componentDidMount() {
+    console.log("Child Component Did Mount");
+  }
 
   render() {
+    console.log("Child Render");
     const { name } = this.props;
     return (
       <div className="user-card">
