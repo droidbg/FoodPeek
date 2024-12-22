5bf967cc07e6d2593f424506c62048e189b75eed
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-22 14:12:57 +0530

    Add state variable in class component

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,14 +3,18 @@ import React from "react";
 class UserCard extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = {
+      count: 0,
+    };
   }
 
   render() {
     const { name } = this.props;
     return (
       <div className="user-card">
+        <h1>Count: {this.state.count}</h1>
         <h2>{name}</h2>
+
         <h3>Location : Dehradun</h3>
         <h4>Contact : @droidbg</h4>
       </div>
