ed72420caa0303b79da8b6cdbc40c50f951237d2
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-30 20:58:56 +0530

    add. custom hook - useOnlineStatus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import logo from "../../images/logo.png";
 import { Link } from "react-router";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const status = useOnlineStatus();
 
   const onClickHandler = () => {
     btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
@@ -18,17 +20,27 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status: {status ? "🟢 " : " 🔴 "}</li>
+
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="link-tag">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" className="link-tag">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact Us</Link>
+            <Link to="/contact" className="link-tag">
+              Contact Us
+            </Link>
           </li>
           <li>
-            <Link to="/cart">Cart</Link>
+            <Link to="/cart" className="link-tag">
+              Cart
+            </Link>
           </li>
           <button className="login-button" onClick={onClickHandler}>
             {btnName}
