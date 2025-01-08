d22958198f0d9e112828e075fa6faf3531743314
Binni G <41873024+droidbg@users.noreply.github.com>
2025-01-08 16:22:22 +0530

    add. lazy loading and suspense for better performance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import About from "./components/About";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+
+// const RestaurantMenu = lazy(() => import("./components/RestaurantMenu"));
+const Contact = lazy(() => import("./components/Contact"));
 
 const AppLayout = () => {
   return (
@@ -32,7 +35,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <Contact />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurant/:restaurantId",
