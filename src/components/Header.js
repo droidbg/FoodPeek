00b36d9d45a14060504aa35ed04e2bd4b7105771
Binni G <41873024+droidbg@users.noreply.github.com>
2025-01-10 13:38:46 +0530

    Change color of ribbon

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   };
 
   return (
-    <div className="flex p-4  shadow-lg mb-2  justify-between bg-pink-100">
+    <div className=" flex p-4  shadow-lg mb-2  justify-between bg-pink-200">
       <div className="w-20">
         <Link to="/">
           <img className="logo-img" src={logo} />
