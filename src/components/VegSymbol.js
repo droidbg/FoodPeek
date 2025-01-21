f27413773e092714c688f3e0963ec15c05a0cde8
Binni G <41873024+droidbg@users.noreply.github.com>
2025-01-21 14:26:17 +0530

    veg non veg symbol

---
+++ src/components/VegSymbol.js
@@ -0,0 +1,9 @@
+const VegSymbol = () => {
+  return (
+    <div className="veg flex items-center justify-center border-2 border-solid border-green-500 h-5 w-5 rounded">
+      <span className="absolute w-3 h-3 rounded-2xl bg-[#3be449]"></span>
+    </div>
+  );
+};
+
+export default VegSymbol;
