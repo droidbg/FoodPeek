2c787d80ea3eba9cb12e8a3803cdd2bec0ef573b
Binni G <41873024+droidbg@users.noreply.github.com>
2025-02-21 21:00:18 +0530

    add. contact us page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,145 +1,88 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Us Page</h1>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet. Et nobis voluptatibus id quis temporibus
-          et galisum inventore et eveniet dolor nam optio dolor. Et dolor itaque
-          vel omnis numquam vel eaque aspernatur aut quia molestiae aut dolores
-          numquam At similique placeat ea galisum consectetur. Eum inventore
-          velit At repellat iusto id molestiae galisum sit repellendus inventore
-          id facere minus aut voluptatem porro? A dolores quas in galisum
-          necessitatibus et harum excepturi et aliquam soluta. Et fuga error ut
-          dolorum internos eum placeat deleniti 33 dolores aperiam et aperiam
-          consequatur ea reprehenderit fugit ex nulla error? Aut cupiditate iste
-          sit doloribus nobis et reprehenderit inventore et esse sapiente. Ea
-          sint omnis eum molestias nulla in iusto molestiae. Est expedita
-          repudiandae aut enim perspiciatis et voluptatum expedita sit doloribus
-          voluptatum sit sint vitae qui sunt fuga. At architecto nihil aut
-          doloremque quae quo recusandae aliquam eos fugiat nulla sed unde
-          veritatis aut magni facere sed inventore aliquid. Ut sunt harum nam
-          possimus aspernatur et minima consequatur cum quibusdam harum et quos
-          voluptatem ut maxime laboriosam aut voluptatibus incidunt! Rem
-          dignissimos internos qui maxime odit in consequatur distinctio. Qui
-          ipsa minus et maiores assumenda sed officiis atque est architecto
-          perspiciatis est tempore ipsa sed numquam molestiae. Aut enim porro At
-          neque fugiat eos possimus voluptatum qui inventore reprehenderit. Qui
-          reprehenderit cupiditate non voluptas deleniti sed laboriosam
-          quisquam.{" "}
-        </p>
-        <p>
-          Id deserunt perspiciatis qui expedita aliquid qui quia velit ut omnis
-          dolorem et asperiores harum non quod neque sit error consequatur. Qui
-          sunt praesentium sit molestias voluptatum et voluptates exercitationem
-          et culpa eligendi et omnis iusto aut voluptatum quos ea obcaecati
-          minima. Et iure nihil et voluptatibus assumenda est magnam deserunt in
-          unde molestiae qui unde quasi. Vel saepe galisum ea minus porro ab
-          dignissimos magni vel harum asperiores. Vel veritatis cumque ut nobis
-          veniam et inventore rerum et exercitationem facere et voluptas nihil
-          id nobis tempora et nulla optio. Aut eligendi aliquid eos nobis sequi
-          eum sunt accusamus in omnis similique. Et officiis dicta et obcaecati
-          Quis ab deserunt quia. Nam possimus officiis sed ipsum eligendi et
-          error autem. Non nostrum quos qui dolorem itaque aut sunt possimus sit
-          molestiae neque At minus galisum aut cumque rerum. Et debitis
-          explicabo non obcaecati nulla est expedita repudiandae sed saepe sint
-          est omnis officiis quo illum consectetur ad doloremque unde. In magni
-          sint eos voluptas modi id porro dolores sit neque sint est sunt
-          voluptas? Aut alias molestiae ab modi nihil sit galisum reiciendis et
-          pariatur quos. Est incidunt unde eum accusamus tempora vel dignissimos
-          expedita est tempora ipsa in saepe consequuntur ex consequatur
-          adipisci. Et internos autem aut porro adipisci id velit dolores eos
-          incidunt sint!{" "}
-        </p>
-        <p>
-          Ea nulla tempora ex corporis assumenda et repellendus iusto. Aut modi
-          deserunt non asperiores voluptates vel iure totam sed pariatur modi et
-          exercitationem dolore. Ex illo maiores et sequi eaque ad rerum natus
-          in quisquam vero ex neque doloremque rem veritatis praesentium. Et
-          internos recusandae eos labore atque aut accusamus consequatur sit
-          quam omnis non eius dolor! Ex nulla odio non officia maiores eos autem
-          doloribus qui voluptas sapiente ut earum iure ut expedita voluptatum.
-          Vel ullam expedita est delectus quod ab dolores facere vel voluptas
-          natus a ratione galisum et delectus atque et consequatur nihil. Eos
-          galisum consequuntur et ratione enim qui neque suscipit non quis
-          veniam et voluptatum voluptatibus qui rerum facere. Et maiores tempora
-          sit alias dolorem est corrupti sequi sed consequatur numquam. Et
-          explicabo tenetur ad placeat vero ut voluptate vitae 33 numquam
-          recusandae ut omnis rerum ut ullam molestiae. Cum temporibus optio et
-          quia commodi aut molestiae quod. Aut rerum repudiandae 33 assumenda
-          corporis et voluptatem cumque et voluptas quos ea quibusdam sint! Rem
-          sunt aliquid eum rerum perspiciatis nam dolorem autem eum architecto
-          ducimus eos officia porro. Eum placeat dolores At eveniet possimus aut
-          facilis eligendi. Cum omnis nihil aut aliquam aperiam aut fuga debitis
-          ea modi error in vero deserunt et sint provident sed ducimus ducimus.{" "}
-        </p>
-        <p>
-          Et sint vitae non ducimus excepturi sed unde galisum. Ut quam
-          laudantium aut dolorum dolores eos enim laborum ab sequi doloribus.
-          Est voluptatum ducimus in eveniet commodi At cupiditate repellendus ab
-          autem placeat quo voluptate animi. Est voluptatibus optio rem dolores
-          galisum et itaque eius. Est quod velit qui sunt magni in nihil
-          aspernatur aut iure dolor quo nobis explicabo. Aut omnis aperiam et
-          dolorum unde sit illo voluptas 33 nihil unde rem voluptas fugiat. Et
-          voluptatibus velit sed deserunt accusantium qui voluptatem aliquam et
-          autem voluptatem qui sunt unde. Non eius quisquam non similique nisi
-          sit nobis quos. In molestiae doloribus qui rerum possimus sit amet
-          commodi ea deleniti laudantium! A voluptatem molestiae qui nostrum
-          ipsam ea consequuntur nulla et labore nemo in iste ducimus non
-          praesentium cupiditate et perspiciatis natus. Cum animi nesciunt in
-          dolor nihil vel deleniti molestias et laudantium tempora qui officia
-          nesciunt eos fuga nostrum.{" "}
-        </p>
-        <p>
-          Et modi pariatur aut explicabo quas eos fugit voluptas! Ea voluptatem
-          nobis qui officiis quisquam ut quia earum vel magni autem aut fugit
-          quidem ut distinctio repudiandae et molestiae veniam. Et rerum
-          necessitatibus sit maxime autem est assumenda voluptatem et dolorem
-          odit et suscipit eveniet sed optio incidunt et adipisci atque. At
-          sapiente alias cum dolor modi rem error perferendis aut nesciunt
-          cumque qui ipsum culpa! Et dolor omnis et laborum molestiae qui
-          tempore nemo ut incidunt repellendus qui adipisci perferendis. Aut
-          corporis voluptatum aut quas saepe non fugiat consequatur eum
-          consequatur rerum in voluptas modi. Ut fuga quidem sit corrupti earum
-          ex cupiditate aperiam ex numquam nihil in deserunt praesentium ad
-          aspernatur accusamus ut quia voluptatibus. Et natus laboriosam qui
-          voluptates tempore qui quibusdam sint et internos odio aut magni
-          corrupti! Hic expedita architecto quo ducimus sint cum voluptatem
-          temporibus sed recusandae maiores ut quam unde aut quod dolor aut
-          doloribus cumque. At officia voluptatem id ipsa deleniti qui officiis
-          nesciunt aut quisquam facilis. Vel ipsam explicabo nam consequatur
-          laudantium qui deleniti molestiae et officiis mollitia aut pariatur
-          voluptatem.{" "}
-        </p>
-        <p>
-          Qui dolorem voluptas ex voluptates odit ut reiciendis enim aut
-          consequatur iusto. Sit voluptatibus quod et nisi saepe aut dolorem
-          voluptas vel consectetur deserunt. Sit sequi animi aut odio
-          consequatur quo necessitatibus Quis ut maxime corporis. At deserunt
-          fuga qui dolorum mollitia eum quisquam incidunt nam Quis inventore.
-          Qui suscipit amet ut possimus voluptatum est quae ratione et itaque
-          modi in omnis mollitia. Sit voluptatem doloremque sed earum veritatis
-          sed vitae illo aut laborum dolore. Ab doloribus consequatur qui
-          asperiores deleniti in facere asperiores qui dolore velit est voluptas
-          praesentium et tenetur quis. Et natus laboriosam ut quisquam sunt ex
-          inventore velit.{" "}
-        </p>
-        <p>
-          Id voluptatem facilis qui quidem officia et internos aperiam qui atque
-          enim. Est error vero qui odit voluptas et dolore minima id aliquid
-          quia a quis quis rem beatae earum et quidem facere. Ut omnis tenetur
-          sed voluptatum omnis rem omnis totam eum officia itaque ut sunt omnis
-          est quia voluptas qui enim dolore. Aut quod possimus et dolores
-          incidunt eos sapiente dolores. Est corporis explicabo sit earum optio
-          aut consequatur quam aut exercitationem rerum aut commodi ullam ut
-          dicta tenetur. Aut voluptatem excepturi vel voluptatum rerum non nobis
-          nesciunt et modi pariatur et laborum consectetur 33 voluptatum
-          explicabo. Et nisi dolorem et perferendis quibusdam ut doloremque
-          assumenda qui numquam beatae eum impedit consequuntur. Ut
-          necessitatibus nesciunt 33 unde quibusdam non voluptatum voluptate ut
-          consectetur galisum ut tempora sint ad esse molestiae.{" "}
-        </p>
-      </div>
+      <section
+        id="contact"
+        className="py-20     px-4 lg:px-20 transition-colors duration-300"
+      >
+        <div className="w-full flex justify-between flex-col md:flex-row mx-auto items-start gap-12">
+          <div
+            className="w-full md:w-5/12"
+            style={{ opacity: 1, transform: "none" }}
+          >
+            <p className="inline-block overflow-hidden text-zinc-900  text-5xl md:text-6xl lg:text-8xl  leading-tight lg:mr-6 lg:mb-8 mr-3">
+              {"Let's".split("").map((char, index) => (
+                <span
+                  key={index}
+                  className="inline-block transform will-change-transform will-change-opacity font-sans font-bold"
+                  style={{
+                    opacity: 1,
+                    transform: "translate3d(0px, 0px, 0px)",
+                  }}
+                >
+                  {char}
+                </span>
+              ))}
+            </p>
+            <p className="inline-block overflow-hidden text-zinc-900  text-5xl md:text-6xl lg:text-8xl font-bold leading-tight lg:mb-8">
+              {"Connect".split("").map((char, index) => (
+                <span
+                  key={index}
+                  className="inline-block transform will-change-transform will-change-opacity font-sans font-bold"
+                  style={{
+                    opacity: 1,
+                    transform: "translate3d(0px, 0px, 0px)",
+                  }}
+                >
+                  {char}
+                </span>
+              ))}
+            </p>
+            <p className="text-neutral-600   text-lg lg:text-2xl my-2 lg:mb-2">
+              Got a question or an idea? I'm just a message away—let's chat and
+              bring it to life!
+            </p>
+          </div>
+          <form className="w-full md:w-6/12 space-y-8 ">
+            {[
+              { name: "name", label: "Name*", type: "text", required: true },
+              { name: "phone", label: "Phone (optional)", type: "tel" },
+              { name: "email", label: "Email*", type: "email", required: true },
+            ].map(({ name, label, type, required }) => (
+              <div key={name} className="relative bg-pink-50 ">
+                <input
+                  type={type}
+                  name={name}
+                  required={required}
+                  className="peer w-full border-b-2 border-pink-300  bg-transparent py-2 px-1  text-pink-800 placeholder-pink-zinc-200    focus:outline-none focus:placeholder-transparent"
+                  placeholder={label}
+                />
+                <label className="absolute left-1 -top-5 text-pink-200 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-pink-400   peer-focus:-top-5 peer-focus:text-sm peer-focus:text-pink-300   ">
+                  {label}
+                </label>
+              </div>
+            ))}
+            <div className="relative   ">
+              <textarea
+                name="message"
+                required
+                rows="2"
+                className="peer w-full border-b-2 border-pink-300  bg-transparent py-2 px-1 text-pink-800 placeholder-transparent    focus:outline-none transition-all resize-none "
+                placeholder="Message"
+              ></textarea>
+              <label className="absolute left-1 -top-5 text-pink-200 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-pink-400   peer-focus:-top-5 peer-focus:text-sm peer-focus:text-pink-300   ">
+                Message*
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="rounded bg-black text-white p-2 font-sans font-bold"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
