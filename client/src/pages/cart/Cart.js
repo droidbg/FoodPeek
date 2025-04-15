b1424df3595a2829dab740918c15817a3b4bd29d
Binni G <41873024+droidbg@users.noreply.github.com>
2025-04-15 13:38:19 +0530

    add. cart coming soon page

--- client/src/pages/cart/Cart.js
+++ client/src/pages/cart/Cart.js
@@ -1,5 +1,11 @@
+import cart from "../../../public/cart.svg";
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="flex items-center">
+      <img src={cart} alt="Restaurant Logo" className="p-5" />
+      <p className="text-5xl"> CART COMING SOON</p>
+    </div>
+  );
 };
 
 export default Cart;
